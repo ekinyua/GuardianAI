0036ec862c1cfae4ccdb9211bc79b4b5d658d853
Frank Onyema Orji <f.orji@alustudent.com>
2024-08-17 01:47:53 +0200

    Worked on sign up

---
+++ mobile/guardian_ai/lib/services/register_auth.dart
@@ -0,0 +1,58 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class RegisterAuth {
+  final String name;
+  final String email;
+  final String password;
+  final String phoneNumber;
+  final String emergencyContact;
+
+  RegisterAuth({
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.phoneNumber,
+    required this.emergencyContact,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'email': email,
+      'password': password,
+      'phoneNumber': phoneNumber,
+      'emergencyContact': emergencyContact,
+    };
+  }
+}
+
+class UserRegistration {
+  static const String apiUrl =
+      'https://guardian-backend-6lro.onrender.com/api/v1/user/register';
+
+  static Future<String?> registerUser(RegisterAuth user) async {
+    try {
+      final response = await http.post(
+        Uri.parse(apiUrl),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(
+          user.toJson(),
+        ),
+      );
+
+      if (response.statusCode == 200) {
+        final responseData = jsonDecode(response.body);
+        return responseData['user_id'];
+      } else {
+        final errorData = jsonDecode(response.body);
+        throw Exception(errorData['Error']);
+      }
+    } catch (e) {
+      print('Error registering user: $e');
+      return null;
+    }
+  }
+}
---
+++ mobile/guardian_ai/lib/utils/colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppCustomColors {
+  static MaterialColor primaryColor = const MaterialColor(
+    0xFF1E1E2D,
+    <int, Color>{
+      50: Color(0xFF1E1E2D),
+      100: Color(0xFF1E1E2D),
+      200: Color(0xFF1E1E2D),
+      300: Color(0xFF1E1E2D),
+      400: Color(0xFF1E1E2D),
+      500: Color(0xFF1E1E2D),
+      600: Color(0xFF1E1E2D),
+      700: Color(0xFF1E1E2D),
+      800: Color(0xFF1E1E2D),
+      900: Color(0xFF1E1E2D),
+    },
+  );
+
+  static Color white = const Color(0xFFFFFFFF);
+  static Color black = const Color(0xFF000000);
+  static Color grey = const Color(0xFFCDCDE0);
+  static Color bluegrey = const Color(0xff1E1E2D);
+  static Color grayishblue = const Color(0xFF7B7B8B);
+  static Color green = const Color(0xFF00C48C);
+}
